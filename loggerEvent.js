140a08014d18835ab17fb221c65729f73332f606
Marco van Duijn <m.v.duijn@gmail.com>
2024-12-30 11:43:18 +0100

    Remove loggerEvent.js file and its associated functionality

--- loggerEvent.js
+++
@@ -1,9 +0,0 @@
-export const loggerEventName = 'logger-event';
-
-export const createLoggerEvent = (message) => {
-    return new CustomEvent(loggerEventName, {
-      detail: message,
-      composed:true,
-      bubbles:true
-    })
-  }
